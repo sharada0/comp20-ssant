215659dc57b5d0e123a17bec9a37b5a3685e5199
Sharada <sharada>
2014-02-25 11:06:31 -0500

    ground sliced in, but not displaying properly

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,17 @@
-// Your work goes here...
+var img = new Image();
+img.src = 'assets/duckhunt.png';
+img.onload = function() {
+
+	draw();
+
+}
+
+function draw() {
+
+	var canvas = document.getElementById('game');
+	var ctx = canvas.getContext('2d');
+
+	ctx.drawImage(img,
+				0, 715, 900, 185, 0, 600, 800, 165);
+
+}
