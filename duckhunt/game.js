99323fcdcb1651dc8cf72f7a9fc9974a6f54419b
Sharada <sharada>
2014-02-26 17:39:51 -0500

    everything except birds is complete (i.e., ground, dog, tree)

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -7,7 +7,21 @@ function draw() {
 	var canvas = document.getElementById('game');
 	var ctx = canvas.getContext('2d');
 
+	//Plain brown dirt road
+	ctx.fillStyle="#C96A1B";
+	ctx.fillRect(0,550,800,50);
+
+	//Tree
 	ctx.drawImage(img,
-				0, 715, 900, 185, 0, 450, 800, 150);
+				0, 270, 105, 130, 0, 50, 270, 420);
+
+	//Topsoil with grass
+	ctx.drawImage(img,
+				0, 715, 900, 185, 0, 310, 800, 250);
+
+	//Dog
+	ctx.drawImage(img,
+				0, 0, 60, 50, 150, 380, 180, 160);
+
 
 }
